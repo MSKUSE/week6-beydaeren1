a82d59b776144323e282c0aa0c87e86023f515ac
beydaeren1 <beydaeren@posta.mu.edu.tr>
2025-03-28 12:22:16 +0300

    Add files via upload

---
+++ src/src/Rectangle.java
@@ -0,0 +1,64 @@
+public class Rectangle extends Shape {
+
+
+
+    private int sideA , sideB;
+
+    public static int counter = 0;
+
+    public int counterForObject=0;
+
+    public Rectangle(Point topLeft, int sideA, int sideB) {
+        super(topLeft);
+        setSideA(sideA);
+        setSideB(sideB);
+        counter ++;
+        counterForObject++;
+    }
+
+    public int getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(int sideA) {
+        if (sideA < 0){
+            this.sideA = 0;
+            throw new IllegalArgumentException("Side cant be negative");
+        }
+        else {
+            this.sideA = sideA;
+        }
+    }
+
+    public int getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(int sideB) {
+        if (sideB < 0) {
+            this.sideB = 0;
+            System.out.println("Side B can't be negative!!");
+        } else {
+            this.sideB = sideB;
+        }
+    }
+
+    @Override
+    public double area() {
+        return this.sideA*this.sideB;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*sideA+2*sideB;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "location=" + getLocation() +
+                ", sideA=" + sideA +
+                ", sideB=" + sideB +
+                '}';
+    }
+}
